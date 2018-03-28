e43a023e2b689b02531ef655279c3fb7a808b71e
ltoedt <laura.toedtli@uzh.ch>
2018-03-28 13:18:29 +0200

    implemented add_card, remove_card methods

--- Export Astah/Position.java
+++ Export Astah/Position.java
@@ -1,15 +1,15 @@
-public class Position {
+public abstract class Position {
 
 	public List<Card> deck;
 
 	private Game game;
 
 	public void add_card(Card card) {
-
+		this.deck.add(card);
 	}
 
 	public void remove_card(Card card) {
-
+		this.deck.remove(card);
 	}
 
 }
