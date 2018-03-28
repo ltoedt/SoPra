f1ce225d93de1819001687bb6a9d4b0ac66426b4
ltoedt <laura.toedtli@uzh.ch>
2018-03-28 13:39:04 +0200

    implemented constructor and buy

--- Export Astah/Market.java
+++ Export Astah/Market.java
@@ -1,13 +1,20 @@
 public class Market extends Position {
 
-	private List[] deck;
+	private List<Card> deck; //better representation for positions
 
 	private Selection selection;
 
 	private Stock stock;
 
-	public void buy(Card card, Player player) {
-
+	public void Market(Game game, List<Card> deck)
+		this.deck = deck;
+		this.selection = game.selection;
+		this.stock = game.stock;
+	
+	public Buy buy(Card card, Player player) {
+		Buy buy = new Buy(card, player);
+		buy.move();
+		return buy;
 	}
 
 	public boolean has_empty_spot() {
