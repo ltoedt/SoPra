d363404ac6b3f4409b4423588a8371cfef2dde1a
ltoedt <laura.toedtli@uzh.ch>
2018-03-28 13:31:40 +0200

    implemented methods
    sacrifice, discard, unselect and constructor --> maybe reconsider what a Position needs and what a Move can do

--- Export Astah/Selection.java
+++ Export Astah/Selection.java
@@ -1,33 +1,41 @@
 public class Selection extends Position {
-
+	
+	private Game game; //vielleicht braucht es nach der Initalisierung Game gar nicht mehr
+	
 	private List<Card> deck;
-
-	private Hand hand;
-
-	private Player[] move cards;
-
-	private Stock trade;
-
-	private Market trade;
-
-	private Sacrifice sacrifice;
-
-	private Selection selection;
-
-	private Selection selection;
-
-	private Stay stay;
-
-	public Sacrifice sacrifice(Card card) {
-		return null;
+	
+	private Graveyard graveyard;
+	
+	private Market market;
+	
+	private Stock stock;
+	
+	public void Selection(Game game, List<Card> deck){
+		this.deck = deck;
+		this.graveyard = game.graveyard;
+		this.market = game.market;
+		this.stock = game.stock;
+		this.game = game;
+	}
+	
+	// Sacrifice als Eiznige Position kein player als input
+	
+	public Sacrifice sacrifice(Card card) { 
+		Sacrifice sac = new Sacrifice(card); //create MoveCard
+		sac.move(); //wenn es so implementiert wird, muss die Position gar nichts von anderen Positions wissen
+		return sac;
 	}
 
 	public Discard discard(Card card, Player player) {
-		return null;
+		Discard dis = new Discard(card, player); // Constructor ist wie rum?
+		dis.move();
+		return dis;
 	}
 
-	public Unselect unselect(Card card) {
-		return null;
+	public Unselect unselect(Card card, Player player) {
+		Unselect unsec = new Unselect(card, player);
+		unsec.move();
+		return unsec;
 	}
 
 }
